1f9662540a895e80f1fd86e3a90e29ccf2f063e4
FATE4869 <zheng94@uw.edu>
2022-11-09 13:35:59 -0800

    add evaluate to hyperpruning.py

--- sources/hyperpruning.py
+++ sources/hyperpruning.py
@@ -2,6 +2,8 @@ from first_round import *
 from main_pruning_competition import future_rounds
 import argparse
 import sys
+from train import train_main
+from args import Args
 
 def main(args):
     parser = argparse.ArgumentParser(description="Hyperpruning")
@@ -12,16 +14,22 @@ def main(args):
     parser.add_argument("-ei", type=int, default=3, required=False, help="number of epochs for the future rounds")
     parser.add_argument("--initial_indices", nargs="*", type=int, default=None)
     parser.add_argument("--hp_opt", type=str, default='tpe', required=False)
+    parser.add_argument("--evaluate", type=str, default='../models/LSTM_PTB_pruned/___e63___117.pt', required=False)
     print(f"args: {args}")
     code_args = parser.parse_args(args)
     print(f"code_args: {code_args}")
     count = code_args.starting_idx
-    code_args.initial_indices = [3]
-    # trials = round1(num_epochs=code_args.e0, max_evals=code_args.max_evals,
-    #                 LE_based=code_args.LE_based, count=count)
-    # simplify_trials(trials=trials, max_evals=code_args.max_evals,
-    #                 ind=code_args.starting_idx, LE_based=code_args.LE_based)
-    future_rounds(code_args)
+    code_args.initial_indices = [15]
+    if code_args.evaluate is None:
+        # trials = round1(num_epochs=code_args.e0, max_evals=code_args.max_evals,
+        #                 LE_based=code_args.LE_based, count=count)
+        # simplify_trials(trials=trials, max_evals=code_args.max_evals,
+        #                 ind=code_args.starting_idx, LE_based=code_args.LE_based)
+        future_rounds(code_args)
+    else:
+        args_ = Args().args
+        args_.evaluate = code_args.evaluate
+        train_main(args_)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
